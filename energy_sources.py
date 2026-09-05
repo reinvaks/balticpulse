64d23730c422565e0576d6c2002471e53d63bf52
reinvaks <rein@post.com>
2026-09-05 19:46:23 +0300

    Add files via upload

--- energy_sources.py
+++ energy_sources.py
@@ -13,7 +13,7 @@ from zoneinfo import ZoneInfo
 import pandas as pd
 import requests
 
-BUILD_VERSION = "12.0.0"
+BUILD_VERSION = "13.0.0"
 
 LOG = logging.getLogger(__name__)
 TALLINN = ZoneInfo("Europe/Tallinn")
